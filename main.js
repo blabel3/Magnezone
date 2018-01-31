265fbe63e2acbc0329425e7e7685779e63e8ca2e
Blabel <blakecabel@gmail.com>
2018-01-30 23:41:14 -0600

    Skeleton bot, but it works

---
+++ main.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = require('./token.js');
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login( token.botToken );

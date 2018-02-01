90eb217041104e19a690b95abb4224f26d40f557
Blabel <blakecabel@gmail.com>
2018-01-31 23:00:33 -0600

    Added protection against rolemeto take mod powers

--- src/commands/roleme.js
+++ src/commands/roleme.js
@@ -1,5 +1,17 @@
 exports.run = (client, message, args) => {
   if( message.channel.name.toLowerCase() != 'welcome') return;
   const role = message.member.guild.roles.find(role => role.name.toLowerCase() === args[0].toLowerCase() );
+
+  // Making sure users can't mod themselves.
+  if( role.hasPermission('ADMINISTRATOR') || role.hasPermission('MANAGE_GUILD') || role.hasPermission('BAN_MEMBERS') ) return;
+
+  switch (role.name.toLowerCase()){
+    case 'artists':
+    case 'writers':
+    case 'coders' :
+      message.member.addRole(message.member.guild.roles.find('name', 'Contributors'));
+      break;
+  }
+
   message.member.addRole(role);
 }

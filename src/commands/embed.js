cc0f7af2c2c2512cb2d3f7d465872535116d61ed
Blabel <blakecabel@gmail.com>
2018-02-01 01:18:44 -0600

    done with some embeds in welcome... this will turn into a dm dialogue someday, methinks.

--- src/commands/embed.js
+++ src/commands/embed.js
@@ -1,17 +1,17 @@
 exports.run = (client, message, args) => {
 
   const embed = {
-    "title": "CONTRIBUTOR",
-    "color": message.guild.roles.find(role => role.name.toLowerCase() === 'contributors').color,
-    "description": "Become a contributor to the project! From drawing to writing to coding and everything in between, there's a lot\
- that needs to get done around here. You'll get access to the creation channels where members talk about the ideas they're planning\
- and everything they want to add to make this the best project ever. Be a part of something amazing!",
+    "title": "LURKER",
+    "color": message.guild.roles.find(role => role.name.toLowerCase() === 'lurkers').color,
+    "description": "I can understand if you have other stuff to do and don't want to actually add stuff to the project.\
+ Sometimes just being there and having a good time is enough, plus, you'll get to follow development. You won't get access\
+ to the creation channels, but you still have announcements and general chat to follow development and talk to people. ",
     "footer": {
       "icon_url": "https://i.imgur.com/HGeBf4L.png",
-      "text": "I'll add this to you when you're an artist, a writer, or a coder!"
+      "text": "!roleme lurkers"
     },
     "thumbnail": {
-      "url": "https://i.imgur.com/uoYlMPT.png"
+      "url": "https://i.imgur.com/XAUKaO2.png"
     }
   };
 

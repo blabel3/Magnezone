64020d384facabaed8bca2ceb71a7eb3e4a29353
Blabel <blakecabel@gmail.com>
2018-02-01 01:19:11 -0600

    Took out useless setting to Magnezone (it was necessary to change from Chatot)

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,3 @@
 exports.run = (client) => {
   console.log(`Ready to help in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-  client.user.setUsername("Magnezone");
 }

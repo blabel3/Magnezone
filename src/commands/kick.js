0f0ce2009c3a88990f1afb751707a5a5bbec83c4
Blabel <blakecabel@gmail.com>
2018-02-05 17:41:05 -0600

    Kick command

---
+++ src/commands/kick.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+  let person = message.mentions.members.first();
+  //slice to get rid of the mentions
+  let reason = args.slice(1).join(" ");
+  person.kick(reason);
+}
+
+exports.info = {
+    name: 'kick',
+    usage: 'kick [user] (reason)',
+    description: 'Kicks a user, with an optional reason for logs.',
+    permission: 'moderator'
+};

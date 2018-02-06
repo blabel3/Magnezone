b1af2a1b4a2666cea624359adc65ee681b2d12dc
Blake Abel <blakecabel@gmail.com>
2018-02-06 12:41:39 -0600

    Purging is possible, not for all mods tho

---
+++ src/commands/purge.js
@@ -0,0 +1,64 @@
+exports.run = (client, message, args) => {
+
+  let maxMessages = 10;
+  let deletedMessages = 0;
+
+  console.log("potato");
+
+  if (message.author.id === client.config.ownerID){
+    maxMessages = 1000;
+  } else if (message.member.roles.get(client.config.moderatorID) != undefined){
+    maxMessages = 100;
+  } else {
+    return;
+  }
+
+  console.log(maxMessages);
+
+  if(args.length === 0){
+
+    message.channel.fetchMessages({ limit: maxMessages }).then(channelMessages => {
+      channelMessages.forEach(msg => {
+        msg.delete().catch(console.error);
+        deletedMessages++;
+      });
+    }).then(() => {
+      message.channel.send(`:white_check_mark: Deleted ${deletedMessages} messages!`)
+        .then(response => response.delete(2000));
+    }).catch(console.error);
+
+  } else if (args.length === 1){
+
+    message.channel.fetchMessages({ limit: Math.max(1, Math.min(parseInt(args[0]), maxMessages)) }).then(channelMessages => {
+      channelMessages.forEach(msg => {
+        msg.delete().catch(console.error);
+        deletedMessages++;
+      });
+    }).then(() => {
+      message.channel.send(`:white_check_mark: Deleted ${deletedMessages} messages!`)
+        .then(response => response.delete(2000));
+    }).catch(console.error);
+
+  } else {
+
+    message.channel.fetchMessages({ limit: Math.min(parseInt(args[0]), maxMessages) }).then(messages => {
+      messages.forEach(msg => {
+        if(message.mentions.members.get(msg.author.id) != undefined){
+          msg.delete().catch(console.error);
+          deletedMessages++;
+        }
+      });
+    }).then(() => {
+      message.channel.send(`:white_check_mark: Deleted ${deletedMessages} messages!`)
+        .then(response => response.delete(2000));
+    }).catch(console.error);
+
+  }
+}
+
+exports.info = {
+    name: 'purge',
+    usage: 'purge (number) (mentions)',
+    description: 'Scans the number of messages specified and deletes them, only those from some members if mentions are specified.',
+    permission: 'moderator'
+}

3367983574cdfdb3b7d080687a6ffe86963569e6
Blabel <blakecabel@gmail.com>
2018-02-07 18:33:50 -0600

    Got rid of extra checks

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -1,50 +1,47 @@
 exports.run = (client, message, args) => {
 
-  let deletedMessages = 0;
-
-  console.log("potato");
-
   if (message.author.id != client.config.ownerID && message.member.roles.get(client.config.moderatorID) == undefined) return;
 
   const maxMessages = 100;
+  let deletedMessages = 0;
 
   if(args.length === 0){
 
     message.channel.fetchMessages({ limit: maxMessages }).then(channelMessages => {
       channelMessages.forEach(msg => {
-        msg.delete().catch(console.error);
+        msg.delete();
         deletedMessages++;
       });
     }).then(() => {
       message.channel.send(`:white_check_mark: Deleted ${deletedMessages} messages!`)
         .then(response => response.delete(3000));
-    }).catch(console.error);
+    });
 
   } else if (args.length === 1){
 
     message.channel.fetchMessages({ limit: Math.max(1, Math.min(parseInt(args[0]), maxMessages)) }).then(channelMessages => {
       channelMessages.forEach(msg => {
-        msg.delete().catch(console.error);
+        msg.delete();
         deletedMessages++;
       });
     }).then(() => {
       message.channel.send(`:white_check_mark: Deleted ${deletedMessages} messages!`)
         .then(response => response.delete(3000));
-    }).catch(console.error);
+    });
 
   } else {
 
     message.channel.fetchMessages({ limit: Math.min(parseInt(args[0]), maxMessages) }).then(messages => {
       messages.forEach(msg => {
         if(message.mentions.members.get(msg.author.id) != undefined){
-          msg.delete().catch(console.error);
+          msg.delete();
           deletedMessages++;
         }
       });
     }).then(() => {
       message.channel.send(`:white_check_mark: Deleted ${deletedMessages} messages!`)
         .then(response => response.delete(3000));
-    }).catch(console.error);
+    });
 
   }
 }
